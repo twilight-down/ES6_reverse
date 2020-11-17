1cba932e8520d88b26d909e33e60ff2a24908708
twilight-down <www.12550.com@qq.com>
2020-11-17 09:22:00 +0800

    promise练习题

--- tb.js
+++ tb.js
@@ -3,10 +3,17 @@ setTimeout(function() {
 });
 Promise.resolve(function() {
     console.log(2);
-});
+}).then(resolve => {
+    console.log(resolve);
+})
+
 new Promise(function(resolve) {
     console.log(3);
-    resolve();
+    // resolve(console.log('AAAA'));
+    // resolve(console.log('AAA'));
+    resolve(function() {
+        console.log('QWE');
+    })
 }).then(function() {
     console.log(4);
 });
